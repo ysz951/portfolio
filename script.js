0ae9ea91f734156e9cdd88c042f3573fae99feac
ysz951 <shengyang_zhou@baylor.edu>
2020-09-10 19:23:18 -0500

    modified style

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
- 
+
+function goAbout() {
+    $('.about_nav').on('click', function(){
+        $('html, body').animate({
+        scrollTop: $("#About").offset().top
+    }, 500);
+    })
+    
+}
+
+function goProject() {
+    $('.project_nav').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $("#Project").offset().top - 50
+        }, 500);
+    })
+    
+}
+
+function goContact() {
+    $('.contact_nav').on('click', function(){
+        $('html, body').animate({
+        scrollTop: $("#Contact").offset().top - 50
+    }, 500);
+    })   
+}
+
+ // main function
+function mainFunc(){
+    goAbout();
+    goProject();
+    goContact();
+  }
+  
+  $(mainFunc);
